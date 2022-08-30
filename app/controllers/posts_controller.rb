44866639321173b100cfdb6a5389a501268d38a5
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-30 09:49:41 -0700

    done with the assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
-    post = Post.find(params[:id])
-    
+    post = post_find
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = post_find
+    post.update!(post_params)
     render json: post
   end
 
@@ -19,5 +19,19 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+
+  def post_find
+    Post.find(params[:id])
+  end
+
+  def render_unprocessable_entity_response (invalid)
+    render json: { errors: invalid.record.errors }, 
+    status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Post not found" }, status: :render_not_found_response
+  end
+
 
 end
